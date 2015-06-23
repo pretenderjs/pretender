06a481e1cc80ac4ca2b0047d6d54f06d53594fd2
simon <ihmig@kaliber5.de>
2015-06-23 20:04:49 +0200

    Fix wrong invocation af dispatchEvent method for passthrough requests. This fixes event listeners added via addEventListener() not being called.

--- pretender.js
+++ pretender.js
@@ -81,7 +81,7 @@ function interceptor(pretender) {
           }
         }
         // fire fake events where applicable
-        fakeXHR.dispatchEvent(evt, e);
+        fakeXHR.dispatchEvent(e);
         if (fakeXHR['on' + evt]) {
           fakeXHR['on' + evt](e);
         }
