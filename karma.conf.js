26b42fc56731667be4dabfbdeb8de65fb1539c2a
Thomas Wang <xwang5@linkedin.com>
2018-05-22 18:24:04 -0700

    add ponyfill

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'node_modules/es6-promise/dist/es6-promise.auto.js',
       'node_modules/abortcontroller-polyfill/dist/abortcontroller-polyfill-only.js',
-      'node_modules/yetch/dist/yetch-ponyfill.js',
+      'assets/yetch-ponyfill.js',
       'pretender.js',
       'test/**/*.js'
     ],
