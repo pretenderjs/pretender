7e35f2b021ae48c328defb58bc4ed1f20426a0eb
Gastón I. Silva <givanse@gmail.com>
2018-12-29 09:58:19 -0800

    add a build step with rollup

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const typescript = require('rollup-plugin-typescript');
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+
+const selfId = path.resolve(__dirname, 'src/iife-self-placeholder.ts');
+
+module.exports = {
+  input: 'src/index.ts',
+  external: [
+    selfId,
+    '@xg-wang/whatwg-fetch',
+    'fake-xml-http-request',
+    'route-recognizer',
+  ],
+  output: [
+    {
+      name: 'Pretender',
+      file: 'pretender.js',
+      format: 'iife',
+      globals: {
+        [selfId]: 'self',
+        '@xg-wang/whatwg-fetch': 'FakeFetch',
+        'fake-xml-http-request': 'FakeXmlHttpRequest',
+        'route-recognizer': 'RouteRecognizer',
+      },
+      banner: 'var self = window;\n' +
+              'var FakeFetch = self.WHATWGFetch;\n' +
+              'var FakeXMLHttpRequest = self.FakeXMLHttpRequest;\n' +
+              'var RouteRecognizer = self.RouteRecognizer;\n',
+    },
+    {
+      file: 'pretender.es.js',
+      format: 'es'
+    },
+    {
+      file: 'pretender.cjs.js',
+      format: 'cjs'
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript()
+  ],
+};
