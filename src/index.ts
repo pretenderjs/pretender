8bc4a4012f888a1306d172af4ba53393383f0223
Gastón I. Silva <givanse@gmail.com>
2019-04-10 16:05:59 -0700

    refactor: extract class Registry

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,7 @@ import RouteRecognizer from 'route-recognizer';
 import FakeXMLHttpRequest from 'fake-xml-http-request';
 import * as FakeFetch from 'whatwg-fetch';
 import parseURL from './parse-url';
-
-/**
- * Registry
- *
- * A registry is a map of HTTP verbs to route recognizers.
- */
-
-function Registry(/* host */) {
-  // Herein we keep track of RouteRecognizer instances
-  // keyed by HTTP method. Feel free to add more as needed.
-  this.verbs = {
-    GET: new RouteRecognizer(),
-    PUT: new RouteRecognizer(),
-    POST: new RouteRecognizer(),
-    DELETE: new RouteRecognizer(),
-    PATCH: new RouteRecognizer(),
-    HEAD: new RouteRecognizer(),
-    OPTIONS: new RouteRecognizer()
-  };
-}
+import Registry from './registry';
 
 /**
  * Hosts
