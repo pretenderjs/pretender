b6c376ed450072bd7c277777e2590f17ff09052c
Sam Selikoff <sam.selikoff@gmail.com>
2019-09-13 17:19:25 -0400

    Back to expected test failure

--- iife-wrapper.js
+++ iife-wrapper.js
@@ -3,8 +3,6 @@ var Pretender = (function(self) {
     return module.default || module;
   }
 
-  var isBrowserLikeEnvironment = typeof window !== 'undefined';
-
   var appearsBrowserified =
     typeof self !== 'undefined' &&
     typeof process !== 'undefined' &&
@@ -15,25 +13,14 @@ var Pretender = (function(self) {
     ? getModuleDefault(require('route-recognizer'))
     : self.RouteRecognizer;
 
-  /*
-    These libraries enable Pretender to work in a Browser. To keep Pretender
-    importable in node, we don't include them if we're in a non-browser-like
-    enviroment. (Note: it's not enough to check if we're running in node, because
-    Pretender works fine in a browser-like node enviroment, e.g. Jest.)
+  var FakeXMLHttpRequest = appearsBrowserified
+    ? getModuleDefault(require('fake-xml-http-request'))
+    : self.FakeXMLHttpRequest;
 
-    In the future if work is done to make Pretender function in node, other
-    node-only deps could be imported in the else block.
-  */
-  if (isBrowserLikeEnvironment) {
-    var FakeXMLHttpRequest = appearsBrowserified
-      ? getModuleDefault(require('fake-xml-http-request'))
-      : self.FakeXMLHttpRequest;
-
-    // fetch related ponyfills
-    var FakeFetch = appearsBrowserified
-      ? getModuleDefault(require('whatwg-fetch'))
-      : self.WHATWGFetch;
-  }
+  // fetch related ponyfills
+  var FakeFetch = appearsBrowserified
+    ? getModuleDefault(require('whatwg-fetch'))
+    : self.WHATWGFetch;
 
   /*==ROLLUP_CONTENT==*/
 
