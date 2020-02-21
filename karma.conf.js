4af5a863a57b53020fd551ea480d94ddbd81d2f4
Thomas Wang <wangxgwxg@gmail.com>
2020-02-20 20:08:45 -0800

    Use headless Chrome in ci

--- karma.conf.js
+++ karma.conf.js
@@ -61,8 +61,15 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
 
+    // you can define custom flags
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
